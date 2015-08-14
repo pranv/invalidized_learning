4d5df16c647db8a539fe353269f6157553c344bf
Pranav Shyam <pranavshyam13@gmail.com>
2015-08-14 23:29:41 +0530

    First Commit
    * Shuffling Works

---
+++ invalidizer/invalidizer.py
@@ -0,0 +1,63 @@
+# OM
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+def split(patch, size):
+	sub_patches = []
+
+	nb_patches_per_dimension = patch.shape[0] / size
+
+	for i in range(nb_patches_per_dimension):
+		for j in range(nb_patches_per_dimension):
+			start_x = j * size
+			start_y = i * size
+			end_x = j * size +  size
+			end_y = i * size + size
+
+			sub_patch = patch[start_x:end_x, start_y:end_y, :]
+			sub_patches.append(sub_patch)
+
+	return sub_patches
+
+def join(patches):
+	nb_patches = len(patches)
+	nb_patches_per_dimension = int(nb_patches ** 0.5)
+
+	rows = []
+
+	for i in range(0, nb_patches, nb_patches_per_dimension):
+		row = np.concatenate(patches[i:i+nb_patches_per_dimension], axis=1)
+		rows.append(row)
+
+	joined_patch = np.concatenate(rows, axis=0)
+
+	return joined_patch
+
+def get_noise_grid(image_length, tile_length, width, std):
+	'''
+		For a given 'tile_size' and 'image_length', generate a gaussian noise 3D tensor that 
+		can be added to the invalid image to smooth out the boundaries.
+	'''
+
+	pass
+
+def invalidizer(image, tile_length, window_size, sigma=1.0):
+	image_length, _, _ = image.shape
+	
+	nb_windows = (image_length / window_size) ** 2
+	nb_tiles = (window_size / tile_length) ** 2
+
+	windows = split(image, window_size)
+
+	for i in range(nb_windows):
+		tiles = split(windows[i], tile_length)
+		tiles = np.random.permutation(tiles)
+		windows[i] = join(tiles)
+
+	invalid_image = join(windows)
+
+	smooth_invalid_image = invalid_image - get_noise_grid(image_length, tile_length, 4, 0.01)
+
+	return smooth_invalid_image
+	#return invalid_image
---
+++ invalidizer/test.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from invalidizer import invalidizer
+'''
+tile_size = 25
+window_size = 100
+image = np.array(Image.open('test.jpg'))
+new_image = invalidizer(image, tile_size, window_size, sigma=1.0)
+
+plt.ion()
+plt.imshow(image)
+raw_input()
+plt.imshow(new_image)
+raw_input()
+'''
+tile_size = 64
+window_size = 128
+image = np.array(Image.open('test2.jpg'))
+new_image = invalidizer(image, tile_size, window_size, sigma=1.0)
+
+plt.ion()
+plt.imshow(image)
+raw_input()
+plt.imshow(new_image)
+raw_input()
